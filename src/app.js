4044d2fcca0ca8360108bc1694eb7204933d7428
bhagatankit05 <bhagatankit82@gmail.com>
2025-06-03 18:28:05 +0530

    utils code

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials: true,
+}));
+app.use(express.json({limit: '20kb'})); //use express.json() to parse JSON bodies 
+app.use(express.urlencoded({extended: true, limit: '20kb'})); //use express.urlencoded() to parse URL-encoded bodies extended is set to true to allow for rich objects and arrays to be encoded into the URL-encoded format
+app.use(express.static('public')); //use express.static() to serve static files from the public directory
+app.use(cookieParser()); //use cookie-parser to parse cookies attached to the client request object
+
+
+
+export {app};
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,15 @@
+class ApiResponse{
+    constructor(statusCode , data , message = "success", errors = [], stack = "") {
+        this.statusCode = statusCode;
+        this.data = data;
+        this.message = message;
+        this.success = statusCode <400;
+        this.errors = errors;
+
+        if (stack) {
+            this.stack = stack;
+        } else {
+            Error.captureStackTrace(this, this.constructor);
+        }
+    }
+}
