10b057f0846dc1c936e31953ad49c1f2111b4353
bhagatankit05 <bhagatankit82@gmail.com>
2025-06-13 15:32:54 +0530

    add user acoount management endpoints

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -260,5 +260,64 @@ const updateAccountDetails = asyncHandler(async (req, res) => {
 }
 )
 
-const updateUserAvatar = asyncHandler()
-export { registerUser, loginUser, logoutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser };
+const updateUserAvatar = asyncHandler(async (req, res) => {
+  const avatarLocalPath = req.file?.path;
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar image is required");
+  }
+  const avatar = await uploadCloudinary(avatarLocalPath);
+  if (!avatar.url) {
+    throw new ApiError(400, "Failed to upload avatar image");
+  }
+  const user = await User.findByIdAndUpdate(
+   req.user?._id,
+    {
+      $set: { avatar: avatar.url }
+    },
+    {
+      new: true
+    }
+  ).select("-password ")
+
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(
+      200,
+      user,
+      "Avatar updated successfully"
+    )
+  );
+}) 
+
+const updateUserCoverImage = asyncHandler(async (req, res) => {
+  const coverImageLocalPath = req.file?.path;
+  if (!coverImageLocalPath) {
+    throw new ApiError(400, "Cover image is required");
+  }
+  const coverimage = await uploadCloudinary(coverImageLocalPath);
+  if (!coverimage.url) {
+    throw new ApiError(400, "Failed to upload cover image");
+  }
+ const user = await User.findByIdAndUpdate(
+   req.user?._id,
+    {
+      $set: { coverimage: coverimage.url }
+    },
+    
+    {
+      new: true
+    }
+  ).select("-password ")
+
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(
+      200,
+      user,
+      "Cover image updated successfully"
+    )
+  );
+}) 
+export { registerUser, loginUser, logoutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateAccountDetails,updateUserAvatar ,updateUserCoverImage};
