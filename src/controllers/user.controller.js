240586f77084eab9549d1dcf054db2a80dd4a3fe
bhagatankit05 <bhagatankit82@gmail.com>
2025-06-13 15:13:32 +0530

    fixed user controller error

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -194,4 +194,71 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     throw new ApiError(401,error?.message || "Invalid refrsh Token")
   }
 })
-export { registerUser, loginUser, logoutUser,refreshAccessToken };
+
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+  const {oldPassword , newPassword} = req.body;
+ const user = await  User.findById(req.user?._id)
+const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+  .then(async (isMatch) => {
+    if (!isPasswordCorrect) {
+      throw new ApiError(400, "Old password is incorrect");
+    }
+    user.password = newPassword;
+    await user.save({validateBeforeSave: false}); // Save the user document without validating the password again
+    return res.status(200).json(
+      new ApiResponse(
+        200,
+        {},
+        "Password changed successfully"
+      )
+    );
+  })
+  .catch((error) => {
+    throw new ApiError(500, error.message || "Failed to change password");
+  });
+})
+const getCurrentUser = asyncHandler(async (req, res) => {
+  
+  return res
+  .status(200)
+  .json(
+      new ApiResponse(
+        200,
+        req.user,
+        "Current user fetched successfully"
+      )
+    
+  );
+})
+const updateAccountDetails = asyncHandler(async (req, res) => { 
+
+  const {fullName,email} = req.body; 
+  if (!fullName || !email) {
+    throw new ApiError(400, "All fields are required");
+  }
+ const user = User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: {
+        fullName,
+        email
+      }
+    },
+    {
+      new: true,
+    }
+  ).select("-password ");
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(
+      200,
+      user,
+      "Account details updated successfully"
+    )
+  );
+}
+)
+
+const updateUserAvatar = asyncHandler()
+export { registerUser, loginUser, logoutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser };
